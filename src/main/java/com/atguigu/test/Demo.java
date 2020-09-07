4f2ba1b08acd6e5f290add654665e4f13836c741
OnApr13 <872126262@qq.com>
2020-09-07 20:39:57 +0800

    gink

---
+++ src/main/java/com/atguigu/test/Demo.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+/**
+ * @program: test
+ * @description:
+ * @author: Mr.dingding
+ * @create: 2020-09-07 20:35* @program: test
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("woshinidie!");
+    }
+}
